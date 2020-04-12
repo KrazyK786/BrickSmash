0fb88e28fa7444386a3c0f833e1aa3f6cdc475ee
Kristopher Johnson <krazyk786@gmail.com>
2020-04-12 17:26:25 -0500

    implemented profile componenet

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -34,43 +34,43 @@ export class AuthService {
     }).pipe(map(res => res));
   }
 
-  // // changed headers to match Angular 6 documentation and added Observable<Data> as return type -https://angular.io/guide/http#http-headers
-  // // commented code represents previous setup for Angular 2
-  // getProfile(): Observable<Data>{
-  //   // let headers = new Headers();
-  //   // formerly headers
-  //   // let headers = new HttpHeaders();
-  //   this.loadToken();
-  //   const httpOptions = {
-  //     headers: new HttpHeaders({
-  //       'Content-Type': 'application/json',
-  //       'Authorization':this.authToken
-  //     })
-  //   }
-  //   // console.log(this.authToken);
-  //   // headers.set('Content-Type', 'application/json');
-  //   // headers.set('Authorization', this.authToken);
-  //   // console.log(headers);
-  //   return this.http.get<Data>('http://localhost:3000/users/profile', httpOptions
-  //     //   {
-  //     //   headers:headers
-  //     // }
-  //   );
-  //   // }).pipe(map(res => res));
-  // }
-  //
+  // changed headers to match Angular 6 documentation and added Observable<Data> as return type -https://angular.io/guide/http#http-headers
+  // commented code represents previous setup for Angular 2
+  getProfile(): Observable<Data>{
+    // let headers = new Headers();
+    // formerly headers
+    // let headers = new HttpHeaders();
+    this.loadToken();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization':this.authToken
+      })
+    }
+    // console.log(this.authToken);
+    // headers.set('Content-Type', 'application/json');
+    // headers.set('Authorization', this.authToken);
+    // console.log(headers);
+    return this.http.get<Data>('http://localhost:8080/users/profile', httpOptions
+      //   {
+      //   headers:headers
+      // }
+    );
+    // }).pipe(map(res => res));
+  }
+
   storeUserData(token, user){
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
     this.authToken = token;
     this.user = user;
   }
-  //
-  // loadToken(){
-  //   const token = localStorage.getItem('id_token');
-  //   this.authToken = token;
-  // }
-  //
+
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
   // loggedIn(): boolean {
   //   // explicitly named token since it no longer looks for id_token by default
   //   // return tokenNotExpired('id_token');
